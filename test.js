41b3a85009edb791192cdfeb576e5320abe5de87
Ganesh <ganeshsaha19sept@gmail.com>
2021-10-23 23:33:44 +0530

    Spread file

---
+++ test.js
@@ -0,0 +1,14 @@
+/*var add=(num1, num2)=>num1+num2;
+
+var array=[123,4,1,1,1];
+
+console.log(add(...array));
+*/
+
+var colors=["Orange","Blue","White"];
+
+var printColors=(c1,c2,c3,c4)=>{
+    console.log(c1,c2,c3,c4);
+}
+
+printColors(...colors);
---
+++ test2.js
@@ -0,0 +1,5 @@
+function myFunction(v,w,x,y,z){
+    console.log(x);
+}
+let args=[0,1];
+myFunction(-1,...args,2,...[3]);
